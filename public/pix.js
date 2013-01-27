068d5aac43467b4f51470755a7218b2de75fff45
TJ Holowaychuk <tj@vision-media.ca>
2013-01-27 13:08:54 -0800

    add start of dropload integration

--- public/pix.js
+++ public/pix.js
@@ -1,2 +1,16 @@
 
+/**
+ * Module dependencies.
+ */
 
+var Dropload = require('dropload');
+
+/**
+ * Drop uploads.
+ */
+
+var drop = Dropload(document.querySelector('#drop'));
+
+drop.on('upload', function(upload){
+  console.log(upload.file);
+});
